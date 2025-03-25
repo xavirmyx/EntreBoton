7777f80a97f4304d1b33186ced6f868ac9eb1650
xavirmyx <xavirmyx@gmail.com>
2025-03-25 23:53:19 +0000

    tiempo

--- index.js
+++ index.js
@@ -411,15 +411,15 @@ bot.on('callback_query', async (query) => {
       },
     });
 
-    // Programar la eliminación del mensaje después de 30 segundos
+    // Programar la eliminación del mensaje después de 10 segundos
     setTimeout(async () => {
       try {
         await bot.deleteMessage(chatId, redirectMessage.message_id);
-        console.log(`✅ Mensaje "Haz clic para ver el contenido" eliminado después de 30 segundos.`);
+        console.log(`✅ Mensaje "Haz clic para ver el contenido" eliminado después de 10 segundos.`);
       } catch (error) {
         console.error(`❌ Error al eliminar el mensaje de redirección: ${error.message}`);
       }
-    }, 30 * 1000); // 30 segundos
+    }, 10 * 1000); // 10 segundos
   } catch (error) {
     console.error('Error al procesar el callback:', error);
     if (error.code === 'ETELEGRAM' && error.response?.body?.description?.includes('query is too old')) {
